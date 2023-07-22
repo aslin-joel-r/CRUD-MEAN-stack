3876b87ed7a83970697ceb3834706f0789ffdc6a
aslin-r <aslinjoelr123@gmail.com>
2023-07-22 09:41:07 +0530

    updated the title

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  content = 'Angualr Js,Lets create a CRUD application';
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,5 +14,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { 
-  
+
 }
