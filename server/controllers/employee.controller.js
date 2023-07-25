50d7fd7728a047cc79f18ede842a791222eb330a
aslin-r <aslinjoelr123@gmail.com>
2023-07-25 12:27:11 +0530

    succesfully added the record to db using the post request

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -3,10 +3,16 @@ const router = express.Router();
 
 const Employee = require('../models/employee.models.js');
 
-router.get('/', (req, res) => { // <-- Corrected order of parameters
+router.get('/', (req, res) => {
   Employee.find()
     .then(data => res.json(data))
     .catch(err => console.log(err))
 });
+
+router.post('/',(req,res)=>{
+   Employee.create(req.body)
+   .then(data=>res.status(201).json(data))
+   .catch(err=>console.log(err))
+})
 
 module.exports = router;
