87b4c0c8c318e1ca550a41e4dded0c43750d3272
aslin-r <aslinjoelr123@gmail.com>
2023-07-25 12:38:00 +0530

    sucessfully retrieve a record with a given id

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const ObjectId=require('mongoose').Types.ObjectId;
 const Employee = require('../models/employee.models.js');
 
 router.get('/', (req, res) => {
@@ -9,6 +10,22 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err))
 });
 
+router.get('/:id', (req, res) => {
+   if(ObjectId.isValid(req.params.id)==false)
+   res.status(400).json({
+      error:"Given Id "+req.params.id+"is not valid"
+   })
+   Employee.findById(req.params.id)
+     .then(data => {
+      if(data)
+         res.send(data)
+      else
+      res.status(404).json({error:"Employee record not found"+req.params.id})
+     })
+     .catch(err => console.log(err))
+ });
+
+ 
 router.post('/',(req,res)=>{
    Employee.create(req.body)
    .then(data=>res.status(201).json(data))
