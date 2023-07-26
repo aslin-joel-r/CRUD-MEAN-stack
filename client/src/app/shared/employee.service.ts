def437ad720ad2a353a43cbfaac7d3b1d90dcfe3
aslin-r <aslinjoelr123@gmail.com>
2023-07-26 22:01:30 +0530

    successfully runned the delete operation

--- client/src/app/shared/employee.service.ts
+++ client/src/app/shared/employee.service.ts
@@ -1,65 +1,61 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { catchError,throwError } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 import { Employee } from './employee.model';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor(private fb:FormBuilder,private http:HttpClient) { }
-  
-  readonly baseUrl='http://localhost:3000/api/employees';
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
-  list:Employee[]=[];
+  readonly baseURL = 'http://localhost:3000/api/employees/';
+  list: Employee[] = [];
 
-  employeeForm=this.fb.group({
-  _id:[''],
-  fullName:['',Validators.required],
-  position:['',Validators.required],
-  location:['',],
-  salary:['',Validators.required],
-  });
+  employeeForm = this.fb.group({
+    _id: [''],
+    fullName: ['', Validators.required],
+    position: ['', Validators.required],
+    location: [''],
+    salary: ['', Validators.required],
+  })
 
-  fetchEmployees(){
-    this.http.get(this.baseUrl)
-    .pipe(catchError(this.errorHandler))
-    .subscribe(data=>{
-      this.list=data as Employee[];
-    })
+  fetchEmployees() {
+    this.http.get(this.baseURL)
+      .pipe(catchError(this.errorHandler))
+      .subscribe(data => {
+        this.list = data as Employee[];
+      })
+
   }
 
-  postEmployee(){
-    return this.http.post(this.baseUrl,this.employeeForm.value)
-    .pipe(catchError(this.errorHandler));
-  }
-  
-  putEmployee(){
-      return this.http.put(this.baseUrl+this.employeeForm.get('_id')?.value,this.employeeForm.value)
+
+  postEmployee() {
+    return this.http.post(this.baseURL, this.employeeForm.value)
       .pipe(catchError(this.errorHandler))
   }
 
-  deleteEmployee(_id:string){
-    return this.http.delete(this.baseUrl + _id)
-    .pipe(catchError(this.errorHandler))
-}
+  putEmployee() {
+    return this.http.put(this.baseURL + this.employeeForm.get('_id')?.value, this.employeeForm.value)
+      .pipe(catchError(this.errorHandler))
+  }
 
-  private errorHandler(error:HttpErrorResponse){
-    if (error.status==0){
-      console.error('An error occurred:',error.error.message);
-    }
-      else{
-        
+  deleteEmployee(_id: string) {
+    return this.http.delete(this.baseURL + _id)
+      .pipe(catchError(this.errorHandler))
+  }
+
+
+  private errorHandler(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
       console.error(
-        `Backend returned code ${error.status},`+
-        `body was: ${error.error}`
-      );
-      }
-    return throwError(()=>new Error('Something bad happened; please try again later.'))
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 
 }
-
