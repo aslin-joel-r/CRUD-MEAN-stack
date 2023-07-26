d838009bbb4157ccea24f212b2093c8dfe815032
aslin-r <aslinjoelr123@gmail.com>
2023-07-26 22:47:36 +0530

    solved the issue in the upadate operation

--- client/src/app/employees/employees.component.ts
+++ client/src/app/employees/employees.component.ts
@@ -1,37 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from '../shared/employee.service';
-import { Employee } from '../shared/employee.model';
 import { ToastrService } from 'ngx-toastr';
+import { Employee } from '../shared/employee.model';
+import { EmployeeService } from '../shared/employee.service';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: []
+  styles: [
+  ]
 })
-export class EmployeesComponent implements OnInit{
+export class EmployeesComponent implements OnInit {
 
-  constructor(public service : EmployeeService,private toastr:ToastrService) { }
+  constructor(public service: EmployeeService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.service.fetchEmployees();
   }
-  
-  populateForm(selectedRecord:Employee){
+
+  populateForm(selectedRecord: Employee) {
     this.service.employeeForm.setValue({
-     
-      _id:selectedRecord._id,
-      fullName:selectedRecord.fullName,
-      position:selectedRecord.position,
-      location:selectedRecord.location,
-      salary:selectedRecord.salary,
+      _id: selectedRecord._id,
+      fullName: selectedRecord.fullName,
+      location: selectedRecord.location,
+      position: selectedRecord.position,
+      salary: selectedRecord.salary,
     })
   }
-  onDelete(_id:string){
-   if(confirm('Are you sure to delete this record?')){
-    this.service.deleteEmployee(_id).subscribe(res=>{
-     this.service.fetchEmployees();
-     this.toastr.error("Deleted Successfully","Employee Register");
-    })
-   }
+
+  onDelete(_id: string) {
+    if (confirm('Are you sure to delete this record?')) {
+      this.service.deleteEmployee(_id).subscribe(res => {
+        this.service.fetchEmployees();
+        this.toastr.error('Deleted successfully', 'Employee Register')
+      })
+    }
   }
+
 }
